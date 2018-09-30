aa38bb4564c10b3fe0eb06d5781550b555709a70
Rohan890 <league@WTS7.attlocal.net>
2018-09-29 17:24:45 -0700

    almost done
    done

--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -17,10 +17,38 @@ public class MergeSorter extends Sorter {
 	
 	private void mergeSort(int[] array, int low, int high, SortingVisualizer display) {
 		//1. Create a temporary integer array that is the same length as the passed in array.
-		
+		int[] temp = new int[array.length];
 		//2. make an if statement that checks if low is less than high
 		//   and put the rest of the method inside of it
-
+if (low < high) {
+	int middle = array.length / 2;
+	mergeSort(array, low, middle, display);
+	mergeSort(array, middle + 1, high, display);
+	for (int i = low; i < high; i++) {
+		temp[i] = array[i];
+	}
+	int i = low;
+	int j = middle + 1;
+	int k = low;
+	while (i <= middle && j <= high) {
+		if (temp[i] <= temp[j]) {
+			array[k] = temp[i];
+			i++;
+		}
+		else {
+			array[k] = temp[j];
+			j++;
+			k++;
+		}
+		while (i <= middle) {
+			array[k] = temp[i];
+			k++;
+			i++;
+		}
+	}
+	
+	
+}
 			//3. Create an integer called middle and set it 
 			//   equal to the half way
             
